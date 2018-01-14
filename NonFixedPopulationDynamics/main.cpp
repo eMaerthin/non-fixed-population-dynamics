f0d0280e20509da651339acc2d741cbcf202311a
Marcin Bodych <marcinbodych@MBP-Marcin.home>
2018-01-14 21:25:25 +0100

    Initial Commit

---
+++ NonFixedPopulationDynamics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NonFixedPopulationDynamics
+//
+//  Created by Marcin Bodych on 14.01.2018.
+//  Copyright (c) 2018 Marcin Bodych. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
